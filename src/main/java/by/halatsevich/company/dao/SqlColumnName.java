c37cb064073f1dc5fd1a0987fad06f0b1c24f8e4
vlad-gal <vlgalathevich@gmail.com>
2020-10-09 16:35:47 +0300

    update 0.1

---
+++ src/main/java/by/halatsevich/company/dao/SqlColumnName.java
@@ -0,0 +1,39 @@
+package by.halatsevich.company.dao;
+
+public class SqlColumnName {
+    public static final String USER_ID = "userId";
+    public static final String EMAIL = "email";
+    public static final String LOGIN = "login";
+    public static final String PASSWORD = "password";
+    public static final String ROLE_ID = "roleId";
+    public static final String ROLE_NAME = "roleName";
+    public static final String STATUS_ID = "statusId";
+    public static final String STATUS_NAME = "statusName";
+    public static final String FIRST_NAME = "firstName";
+    public static final String LAST_NAME = "lastName";
+    public static final String TELEPHONE_NUMBER = "telephoneNumber";
+    public static final String FLIGHT_ID = "flightId";
+    public static final String DEPARTURE_AIRPORT_ID = "departureAirportId";
+    public static final String DESTINATION_AIRPORT_ID = "destinationAirportId";
+    public static final String DEPART_TIME = "departTime";
+    public static final String ARRIVE_TIME = "arriveTime";
+    public static final String AIRCRAFT_ID = "aircraftId";
+    public static final String CREW_ID = "crewId";
+    public static final String OPERATOR_ID = "operatorId";
+    public static final String DISPATCHER_ID = "dispatcherId";
+    public static final String NUMBER_OF_PILOTS = "numberOfPilots";
+    public static final String NUMBER_OF_NAVIGATORS = "numberOfNavigators";
+    public static final String NUMBER_OF_RADIOMAN = "numberOfRadioman";
+    public static final String NUMBER_OF_STEWARDESSES = "numberOfStewardesses";
+    public static final String AIRPORT_ID = "airportId";
+    public static final String AIRPORT_NAME = "airportName";
+    public static final String COUNTRY = "country";
+    public static final String CITY = "city";
+    public static final String AIRCRAFT_TYPE_ID = "aircraftTypeId";
+    public static final String AIRCRAFT_TYPE = "aircraftType";
+    public static final String TAIL_NUMBER = "tailNumber";
+    public static final String AIRCRAFT_NAME = "aircraftName";
+
+
+    public static final String ENTITY_NAME = "name";
+}
