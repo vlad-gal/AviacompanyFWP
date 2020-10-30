e99fb26aad5ec239f5911f53115b609ce2a1f4ed
vlad-gal <vlgalathevich@gmail.com>
2020-10-31 01:12:15 +0300

    update 0.5.3.9.7: registration command

--- src/main/java/by/halatsevich/company/controller/command/impl/RegistrationCommand.java
+++ src/main/java/by/halatsevich/company/controller/command/impl/RegistrationCommand.java
@@ -4,25 +4,25 @@ import by.halatsevich.company.controller.PagePath;
 import by.halatsevich.company.controller.ParameterName;
 import by.halatsevich.company.controller.command.Command;
 import by.halatsevich.company.model.entity.RegistrationData;
+import by.halatsevich.company.model.entity.User;
+import by.halatsevich.company.model.exception.ServiceException;
 import by.halatsevich.company.model.service.ServiceFactory;
 import by.halatsevich.company.model.service.UserService;
-import by.halatsevich.company.model.exception.ServiceException;
 import by.halatsevich.company.util.mail.MailUtil;
 import by.halatsevich.company.validator.UserValidator;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
-import java.io.IOException;
+import java.util.Optional;
 
 public class RegistrationCommand implements Command {
     private static final Logger logger = LogManager.getLogger(RegistrationCommand.class);
 
     @Override
-    public String execute(HttpServletRequest request) throws ServletException, IOException {
+    public String execute(HttpServletRequest request) {
         String login = request.getParameter(ParameterName.LOGIN);
         String password = request.getParameter(ParameterName.PASSWORD);
         String confirmPassword = request.getParameter(ParameterName.CONFIRM_PASSWORD);
@@ -37,10 +37,13 @@ public class RegistrationCommand implements Command {
             ServiceFactory factory = ServiceFactory.getInstance();
             UserService service = factory.getUserService();
             try {
-                if (!service.isUserExist(login, email)) {
+                Optional<User> userByEmail = service.findUserByEmail(email);
+                Optional<User> userByLogin = service.findUserByLogin(login);
+                if (!userByEmail.isPresent() && !userByLogin.isPresent()) {
                     boolean isUserRegistered = service.registration(registrationData);
                     if (isUserRegistered) {
-                        MailUtil.getInstance().sendActivationLink((String) session.getAttribute(ParameterName.LANG), email, request.getRequestURL().toString());
+                        MailUtil.getInstance().sendMessage((String) session.getAttribute(ParameterName.LANG),
+                                email, request.getRequestURL().toString(), MailUtil.MailType.ACTIVATION);
                         page = PagePath.SUCCESSFUL_REGISTRATION;
                     } else {
                         logger.log(Level.WARN, "Cannot register user");
@@ -49,14 +52,14 @@ public class RegistrationCommand implements Command {
                     }
                 } else {
                     logger.log(Level.WARN, "User already exist");
-                    request.setAttribute(ParameterName.USER_EXIST_FLAG, true);
+                    request.setAttribute(ParameterName.USER_ALREADY_EXIST_FLAG, true);
                     page = PagePath.REGISTRATION;
                 }
 
             } catch (ServiceException e) {
                 logger.log(Level.ERROR, "Cannot register user", e);
                 request.setAttribute(ParameterName.ERROR_MESSAGE, e);
-                page = PagePath.ERROR;
+                page = PagePath.ERROR_500;
             }
         } else {
             request.setAttribute(ParameterName.ERROR_VALIDATION_FLAG, true);
