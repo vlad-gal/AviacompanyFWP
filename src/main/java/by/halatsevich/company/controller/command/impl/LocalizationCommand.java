3ec5ae556555fdba8e6495449b8cec1ebec00078
vlad-gal <vlgalathevich@gmail.com>
2020-10-31 00:39:31 +0300

    update 0.5.3.9.4: localization command

--- src/main/java/by/halatsevich/company/controller/command/impl/LocalizationCommand.java
+++ src/main/java/by/halatsevich/company/controller/command/impl/LocalizationCommand.java
@@ -1,30 +1,18 @@
 package by.halatsevich.company.controller.command.impl;
 
 import by.halatsevich.company.controller.ParameterName;
-import by.halatsevich.company.controller.RequestHandler;
 import by.halatsevich.company.controller.command.Command;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
-import java.io.IOException;
-import java.util.Map;
 
 public class LocalizationCommand implements Command {
-    private static final String DEFAULT_LANG = "ru";
 
     @Override
-    public String execute(HttpServletRequest request) throws ServletException, IOException {
+    public String execute(HttpServletRequest request) {
         HttpSession session = request.getSession();
         String lang = request.getParameter(ParameterName.LANG);
-        if (lang == null || lang.isEmpty()) {
-            lang = DEFAULT_LANG;
-        }
         session.setAttribute(ParameterName.LANG, lang);
-        RequestHandler handler = (RequestHandler) session.getAttribute(ParameterName.REQUEST_ATTRIBUTES);
-        for (Map.Entry<String, Object> parameters : handler.getAttributes().entrySet()) {
-            request.setAttribute(parameters.getKey(), parameters.getValue());
-        }
         return (String) session.getAttribute(ParameterName.CURRENT_PAGE);
     }
 }
