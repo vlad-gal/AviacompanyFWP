3d8956b4a9605033d30fe0a83d2748953514de32
vlad-gal <vlgalathevich@gmail.com>
2020-10-31 00:37:59 +0300

    update 0.5.3.9.3: forgot password command

--- src/main/java/by/halatsevich/company/controller/command/impl/ForgotPasswordCommand.java
+++ src/main/java/by/halatsevich/company/controller/command/impl/ForgotPasswordCommand.java
@@ -3,38 +3,49 @@ package by.halatsevich.company.controller.command.impl;
 import by.halatsevich.company.controller.PagePath;
 import by.halatsevich.company.controller.ParameterName;
 import by.halatsevich.company.controller.command.Command;
+import by.halatsevich.company.model.entity.User;
 import by.halatsevich.company.model.exception.ServiceException;
 import by.halatsevich.company.model.service.ServiceFactory;
 import by.halatsevich.company.model.service.UserService;
 import by.halatsevich.company.util.mail.MailUtil;
 import by.halatsevich.company.validator.UserValidator;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
-import java.io.IOException;
+import javax.servlet.http.HttpSession;
+import java.util.Optional;
 
 public class ForgotPasswordCommand implements Command {
+    private static final Logger logger = LogManager.getLogger(ForgotPasswordCommand.class);
+
     @Override
-    public String execute(HttpServletRequest request) throws ServletException, IOException {
+    public String execute(HttpServletRequest request) {
+        HttpSession session = request.getSession();
         String email = request.getParameter(ParameterName.EMAIL);
         String page;
         if (UserValidator.isValidEmail(email)) {
             ServiceFactory factory = ServiceFactory.getInstance();
             UserService service = factory.getUserService();
             try {
-                if (service.isEmailExist(email)) {
-                    MailUtil.getInstance().sendResetPasswordLink((String) request.getAttribute(ParameterName.LANG), email, request.getRequestURL().toString());
+                Optional<User> user = service.findUserByEmail(email);
+                if (user.isPresent()) {
+                    MailUtil.getInstance().sendMessage((String) session.getAttribute(ParameterName.LANG),
+                            email, request.getRequestURL().toString(), MailUtil.MailType.RESET);
                     page = PagePath.RESET_PASSWORD_LINK_SENT;
                 } else {
-                    request.setAttribute("emailNotExistFlag", true);
+                    request.setAttribute(ParameterName.EMAIL_NOT_EXIST_FLAG, true);
                     page = PagePath.FORGOT_PASSWORD;
                 }
             } catch (ServiceException e) {
+                logger.log(Level.ERROR, "Error while checking email", e);
                 request.setAttribute(ParameterName.ERROR_MESSAGE, e);
-                page = PagePath.ERROR;
+                page = PagePath.ERROR_500;
             }
         } else {
-            request.setAttribute("incorrectEmailFlag",true);
+            logger.log(Level.WARN, "Incorrect email");
+            request.setAttribute(ParameterName.INCORRECT_EMAIL_FLAG, true);
             page = PagePath.FORGOT_PASSWORD;
         }
         return page;
