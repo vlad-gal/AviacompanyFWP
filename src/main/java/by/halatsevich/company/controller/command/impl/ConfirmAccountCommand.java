2ae1ed6092a5fb72534c691543651e31281f66e9
vlad-gal <vlgalathevich@gmail.com>
2020-10-31 00:36:24 +0300

    update 0.5.3.9.2: confirm account command

--- src/main/java/by/halatsevich/company/controller/command/impl/ConfirmAccountCommand.java
+++ src/main/java/by/halatsevich/company/controller/command/impl/ConfirmAccountCommand.java
@@ -4,6 +4,7 @@ import by.halatsevich.company.controller.PagePath;
 import by.halatsevich.company.controller.ParameterName;
 import by.halatsevich.company.controller.command.Command;
 import by.halatsevich.company.model.entity.Status;
+import by.halatsevich.company.model.entity.User;
 import by.halatsevich.company.model.exception.ServiceException;
 import by.halatsevich.company.model.service.ServiceFactory;
 import by.halatsevich.company.model.service.UserService;
@@ -12,34 +13,34 @@ import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
-import java.io.IOException;
+import java.util.Optional;
 
 public class ConfirmAccountCommand implements Command {
     private static final Logger logger = LogManager.getLogger(ConfirmAccountCommand.class);
 
     @Override
-    public String execute(HttpServletRequest request) throws ServletException, IOException {
-        ServiceFactory factory = ServiceFactory.getInstance();
-        UserService service = factory.getUserService();
+    public String execute(HttpServletRequest request) {
         String email = request.getParameter(ParameterName.EMAIL);
         String page;
-        if (UserValidator.isValidEmail(email)){
+        if (UserValidator.isValidEmail(email)) {
+            ServiceFactory factory = ServiceFactory.getInstance();
+            UserService service = factory.getUserService();
             try {
-                if (service.isEmailExist(email) && service.updateUserStatus(email, Status.ACTIVE)) {
+                Optional<User> user = service.findUserByEmail(email);
+                if (user.isPresent() && service.updateUserStatus(email, Status.ACTIVE)) {
                     page = PagePath.SUCCESSFUL_ACTIVATION;
                 } else {
                     page = PagePath.REGISTRATION;
                 }
             } catch (ServiceException e) {
-                logger.log(Level.ERROR, "Error while updating users status");
-                request.setAttribute(ParameterName.ERROR_MESSAGE,e);
-                page = PagePath.ERROR;
+                logger.log(Level.ERROR, "Error while updating users status", e);
+                request.setAttribute(ParameterName.ERROR_MESSAGE, e);
+                page = PagePath.ERROR_500;
             }
         } else {
             logger.log(Level.WARN, "Invalid email");
-            page = PagePath.ERROR;
+            page = PagePath.ERROR_404;
         }
         return page;
     }
