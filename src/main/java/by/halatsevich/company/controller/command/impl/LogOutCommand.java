5c3b75971b53f1fcdb6882e4288a77b97e7edd55
vlad-gal <vlgalathevich@gmail.com>
2020-10-31 00:39:53 +0300

    update 0.5.3.9.5: log out command

--- src/main/java/by/halatsevich/company/controller/command/impl/LogOutCommand.java
+++ src/main/java/by/halatsevich/company/controller/command/impl/LogOutCommand.java
@@ -3,17 +3,13 @@ package by.halatsevich.company.controller.command.impl;
 import by.halatsevich.company.controller.PagePath;
 import by.halatsevich.company.controller.command.Command;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
-import java.io.IOException;
 
 public class LogOutCommand implements Command {
 
     @Override
-    public String execute(HttpServletRequest request) throws ServletException, IOException {
-        HttpSession session = request.getSession();
-        session.invalidate();
+    public String execute(HttpServletRequest request) {
+        request.getSession().invalidate();
         return PagePath.INDEX;
     }
 }
