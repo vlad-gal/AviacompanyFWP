d7df0cf61eafcb312131ae2647bfd8b37d1c6756
vlad-gal <vlgalathevich@gmail.com>
2020-10-31 00:40:21 +0300

    update 0.5.3.9.6: wrong action command

--- src/main/java/by/halatsevich/company/controller/command/impl/WrongActionCommand.java
+++ src/main/java/by/halatsevich/company/controller/command/impl/WrongActionCommand.java
@@ -3,14 +3,12 @@ package by.halatsevich.company.controller.command.impl;
 import by.halatsevich.company.controller.PagePath;
 import by.halatsevich.company.controller.command.Command;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
-import java.io.IOException;
 
 public class WrongActionCommand implements Command {
 
     @Override
-    public String execute(HttpServletRequest request) throws ServletException, IOException {
-        return PagePath.NOT_FOUND_COMMAND;
+    public String execute(HttpServletRequest request) {
+        return PagePath.ERROR_404;
     }
 }
