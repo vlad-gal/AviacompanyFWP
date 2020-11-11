afd69e29ff5a130dc5e25fc6ea3c4c5c1743a893
vlad-gal <vlgalathevich@gmail.com>
2020-11-11 17:08:21 +0300

    update 0.6: first working version

---
+++ src/main/java/by/halatsevich/company/controller/command/impl/AllFlightsCommand.java
@@ -0,0 +1,47 @@
+package by.halatsevich.company.controller.command.impl;
+
+import by.halatsevich.company.controller.PagePath;
+import by.halatsevich.company.controller.ParameterName;
+import by.halatsevich.company.controller.command.Command;
+import by.halatsevich.company.model.entity.Flight;
+import by.halatsevich.company.model.exception.ServiceException;
+import by.halatsevich.company.model.service.FlightService;
+import by.halatsevich.company.model.service.ServiceFactory;
+import by.halatsevich.company.validator.BaseValidator;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.List;
+
+public class AllFlightsCommand implements Command {
+    private static final Logger logger = LogManager.getLogger(AllFlightsCommand.class);
+
+    @Override
+    public String execute(HttpServletRequest request) {
+        String status = request.getParameter(ParameterName.STATUS);
+        HttpSession session = request.getSession();
+        String page;
+        if (BaseValidator.isValidStatus(status)) {
+            ServiceFactory factory = ServiceFactory.getInstance();
+            FlightService service = factory.getFlightService();
+            try {
+                List<Flight> flightList = service.findFlightsByStatus(status);
+                session.setAttribute(ParameterName.CURRENT_PAGE_NUMBER, 1);
+                session.setAttribute(ParameterName.FLIGHT_LIST, flightList);
+                request.setAttribute(ParameterName.SHOW_FLIGHTS_FLAG, true);
+                page = PagePath.USER_ACCOUNT;
+            } catch (ServiceException e) {
+                logger.log(Level.ERROR, "Error while finding all flights by status", e);
+                request.setAttribute(ParameterName.ERROR_MESSAGE, e);
+                page = PagePath.ERROR_500;
+            }
+        } else {
+            logger.log(Level.ERROR, "Invalid status");
+            page = PagePath.ERROR_404;
+        }
+        return page;
+    }
+}
